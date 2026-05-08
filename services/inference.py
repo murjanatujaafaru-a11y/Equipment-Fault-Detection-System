934886991cdf3eda36a5e81d29bf63f519d87af6
murjanatujaafaru@gmail.com <murjanatujaafaru@gmail.com>
2026-05-08 15:00:41 +0100

    chage path in line 3

--- services/inference.py
+++ services/inference.py
@@ -1,5 +1,11 @@
 import torch
 import torch.nn.functional as F
+import sys
+import os
+
+# Ensure the root directory is in the path for this sub-module too
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from model.model import build_model
 import gdown
 import json
