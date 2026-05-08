1cc15313b9362551bcd40f7dcefa203c74bdc98c
murjanatujaafaru@gmail.com <murjanatujaafaru@gmail.com>
2026-05-08 03:13:59 +0100

    add __init__

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import streamlit as st
 from PIL import Image
 
-from services.inference import load_model, predict
+from services.inference import __init__, predict
 from utils.preprocessing import preprocess
 
 
